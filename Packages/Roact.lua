4e0580d9f445b30b20f471df4735491899a73fe3
Pyseph <vlade.lind@gmail.com>
2022-07-26 20:55:04 +0200

    Remove Packages and releasebuild.rbxm

--- Packages/Roact.lua
+++
@@ -1,1 +0,0 @@
-return require(script.Parent._Index["pysephwasntavailable_roact-constant-fork@1.5.0"]["roact-constant-fork"])
--- Packages/_Index/pysephwasntavailable_roact-constant-fork@1.5.0/roact-constant-fork/src/PropMarkers/Constant.lua
+++
@@ -1,7 +0,0 @@
-local Symbol = require(script.Parent.Parent.Symbol)
-
-local Constant = {
-	SkipBindingUpdate = Symbol.named("SkipBindingUpdate"),
-}
-
-return Constant
